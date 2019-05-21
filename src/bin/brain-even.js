1bc444b46405315ba735828cdcb291eebc036f54
Valery Kovalev <robinsout@gmail.com>
2019-05-21 17:34:23 +0300

    Added Brain-even game

---
+++ src/bin/brain-even.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+import { checkEven } from '..';
+
+checkEven();
--- src/index.js
+++ src/index.js
@@ -1,8 +1,47 @@
 import readlineSync from 'readline-sync';
 
-const functionHello = () => {
+export const functionHello = () => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('What is your name? ');
-  console.log('Hello,', userName, '!');
+  console.log(`Hello, ${userName}!`);
+  return userName;
 };
-export default functionHello;
+
+const getRandomInteger = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+
+const isEven = num => (num % 2 === 0);
+
+const askUserAndCheckEven = () => {
+  const randomNum = getRandomInteger(1, 100);
+  console.log('Question:', randomNum);
+  const userAnswer = readlineSync.question('Your answer: ');
+  let correctAnswer = '';
+  if (isEven(randomNum)) {
+    correctAnswer = 'yes';
+  } else {
+    correctAnswer = 'no';
+  }
+  return [userAnswer, correctAnswer];
+};
+
+export const checkEven = () => {
+  const userName = functionHello();
+  console.log('Answer "yes" if number even otherwise answer "no".');
+  let i = 0;
+  let ask;
+  do {
+    ask = askUserAndCheckEven();
+    if (ask[0].toLowerCase() === ask[1]) {
+      console.log('Correct!');
+    } else {
+      console.log(`'${ask[0]}' is wrong answer ;(. Correct answer was '${ask[1]}'`);
+      console.log(`Let's try again, ${userName}!`);
+      break;
+    }
+    i += 1;
+  }
+  while (i < 3 && ask[0] !== false);
+  if (i === 3) {
+    console.log(`Congratulations, ${userName}!`);
+  }
+};

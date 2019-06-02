6e28a3f28e23eda522b44b77c97c602342c23757
Valery Kovalev <robinsout@gmail.com>
2019-06-02 23:54:06 +0300

    CodeClimate recommendations applied

--- src/games/progression.js
+++ src/games/progression.js
@@ -3,20 +3,25 @@ import gameProcess from '..';
 
 const gameDescription = 'What number is missing in the progression?';
 
+const generateProgression = () => {
+  const difference = generateNumber(-20, 20);
+  const numberOfTerms = 10;
+  const initialTerm = generateNumber(-20, 20);
+  const progressionGenerator = a => a + difference;
+  const generatedProgression = [initialTerm];
+  for (let j = 0; j < (numberOfTerms - 1); j += 1) { // Filling progression array
+    const newElement = progressionGenerator(generatedProgression[j]);
+    generatedProgression.push(newElement);
+  }
+  return generatedProgression;
+};
+
 const progressionGame = (gameStarted = false) => {
   let result;
   if (gameStarted === false) { // Game not started yet, passing game function to the engine
     result = gameProcess(progressionGame);
   } else if (gameStarted === true) { // Game already started, passing game data to the engine
-    const difference = generateNumber(-20, 20);
-    const numberOfTerms = 10;
-    const initialTerm = generateNumber(-20, 20);
-    const progressionGenerator = a => a + difference;
-    const generatedProgression = [initialTerm];
-    for (let j = 0; j < (numberOfTerms - 1); j += 1) { // Filling progression array
-      const newElement = progressionGenerator(generatedProgression[j]);
-      generatedProgression.push(newElement);
-    }
+    const generatedProgression = generateProgression();
     const hiddenElementIndex = generateNumber(0, generatedProgression.length - 1);
     const hiddenElement = generatedProgression[hiddenElementIndex];
     generatedProgression[hiddenElementIndex] = '..';

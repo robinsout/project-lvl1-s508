c9111201d9077bdfa6263c9463aa68168709ef6e
Valery Kovalev <robinsout@gmail.com>
2019-06-03 12:16:29 +0300

    Cognitive complexity decreased in prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -3,20 +3,20 @@ import gameProcess from '..';
 
 const gameDescription = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
-const primeGame = (gameStarted = false) => {
-  const isPrime = (num) => {
-    let counter = 0;
-    for (let i = 1; i <= num / 2; i += 1) {
-      if (num % i === 0) {
-        counter += 1;
-      }
+const isPrime = (num) => {
+  let counter = 0;
+  for (let i = 1; i <= num / 2; i += 1) {
+    if (num % i === 0) {
+      counter += 1;
     }
-    if (counter === 1) {
-      return true;
-    }
-    return false;
-  };
+  }
+  if (counter === 1) {
+    return true;
+  }
+  return false;
+};
 
+const primeGame = (gameStarted = false) => {
   // If game not started yet, passing game function to the engine
   // If no, game already started, passing game data to the engine
   if (gameStarted === false) {
